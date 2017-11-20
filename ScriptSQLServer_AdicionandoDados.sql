2986167eae9b076579246bfeb5da99069ed3da35
Priscilla Amaro <primelory@hotmail.com>
2017-11-20 20:03:47 -0200

    Evoluindo script de dados padrões

--- ScriptSQLServer_AdicionandoDados.sql
+++ ScriptSQLServer_AdicionandoDados.sql
@@ -4,6 +4,20 @@ insert into TIPO_USUARIO values (2, 'Atendente');
 insert into TIPO_USUARIO values (3, 'Desenvolvedor');
 
 select * from USUARIO;
-insert into USUARIO values ('cteste', 123, 1, 'Cliente de teste');
-insert into USUARIO values ('ateste', 123, 2, 'Atendente de teste');
-insert into USUARIO values ('dteste', 123, 3, 'Desenvolvedor de teste');
+insert into USUARIO (login, senha, ID_TIPO_USUARIO, nome) values ('cteste', 123, 1, 'Cliente de teste');
+insert into USUARIO (login, senha, ID_TIPO_USUARIO, nome) values ('ateste', 123, 2, 'Atendente de teste');
+insert into USUARIO (login, senha, ID_TIPO_USUARIO, nome) values ('dteste', 123, 3, 'Desenvolvedor de teste');
+
+select * from PRIORIDADE_CHAMADO;
+insert into PRIORIDADE_CHAMADO values (1, 'Baixo');
+insert into PRIORIDADE_CHAMADO values (2, 'Médio');
+insert into PRIORIDADE_CHAMADO values (3, 'Alto');
+
+select * from STATUS_CHAMADO;
+insert into STATUS_CHAMADO values (1, 'Aberto');
+insert into STATUS_CHAMADO values (2, 'Em análise');
+insert into STATUS_CHAMADO values (3, 'Encaminhado para correção');
+insert into STATUS_CHAMADO values (4, 'Em correção');
+insert into STATUS_CHAMADO values (5, 'Em validação');
+insert into STATUS_CHAMADO values (6, 'Reaberto');
+insert into STATUS_CHAMADO values (7, 'Fechado');
